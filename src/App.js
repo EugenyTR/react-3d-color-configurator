01cc5b588d419de1ff8ce7eca9629fdef397d554
Eugeny Semenov <et@takeseoeasy.com>
2021-01-05 22:54:55 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -71,7 +71,10 @@ function Picker() {
   return (
     <div style={{ display: snap.current ? "block" : "none" }}>
       <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
-      <h1>{snap.current}</h1>
+      <h1>
+        {/*{snap.current}*/}
+        Меняй цвет!
+      </h1>
     </div>
   )
 }
